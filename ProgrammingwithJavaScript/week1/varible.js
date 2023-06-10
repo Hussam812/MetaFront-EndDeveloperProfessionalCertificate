523cdc4582aade2679c1010ed6035cc104a7caac
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-10 14:28:44 +0200

    varibles

---
+++ ProgrammingwithJavaScript/week1/varible.js
@@ -0,0 +1,9 @@
+var person ; //declar a varible
+person  = "john"; //assin a value of name "john" to varible person
+console.log("Hello", person);
+// create another varible
+var greeting = "Hello";
+console.log(greeting, person);
+greeting ="hi";
+person = "james";
+console.log(greeting, person)
