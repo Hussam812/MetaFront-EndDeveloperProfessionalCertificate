5b92adae31fca9674f9d1ab7856bac54002df64f
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-10 23:04:27 +0200

    opertors in depth

---
+++ ProgrammingwithJavaScript/week1/opertorsindeph.js
@@ -0,0 +1,44 @@
+var currentTime = 10;
+console.log(currentTime > 9 && currentTime < 17);
+
+console.log(10 > 9 && 10 < 17);
+
+console.log(true && true);
+
+
+
+currentTime = 7 ;
+console.log(currentTime < 9 || currentTime > 17);
+console.log(true || false);
+
+
+var petHungry = true;
+console.log("Pet is hungry: ", petHungry);
+console.log("Feeding the pet");
+console.log("Pet is hungry: ", !petHungry);
+console.log(petHungry);
+
+
+console.log(22 % 5); // 2
+
+console.log(5 !== "5");
+
+
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime +=2;
+overtime += 3;
+console.log(overtime);
+
+
+var longString = "";
+longString += "Once";
+longString += " upon";
+longString += " a";
+longString += " time";
+longString += "...";
+console.log(longString);
+
+
+var num = 10;
