9d1042abb63316ec1f118f87ec4855c000b7f394
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 09:26:48 +0200

    nested loop

---
+++ ProgrammingwithJavaScript/week1/nestedloops.js
@@ -0,0 +1,13 @@
+for(i = 1; i<= 2; i++){
+    for (j =1; j <=5; j++){
+        console.log("Week " + i + " day " +j) ;
+
+    }
+}
+
+for( var i = 2023; i < 2025; i++){
+    console.log(i);
+    for(var j = 6; j < 9; j++){
+        console.log("------", j)
+    }
+}
