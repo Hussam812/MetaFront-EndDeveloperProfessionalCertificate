d0411239e139fa753ea2a01cafc6c89172dec5a5
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 08:54:35 +0200

    for loop

---
+++ ProgrammingwithJavaScript/week1/forloop.js
@@ -0,0 +1,14 @@
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("Go!");
+for (var i = 1; i<= 3; i++){
+    console.log(i);
+}
+console.log("Go!");
+
+for (var i = 10; i > 0; i--){
+    console.log(i);
+}
+console.log("Happy New Year!");
