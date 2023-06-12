ad00f7b04a02832e631596c98c8db6b3f83abf65
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 15:14:39 +0200

    closer to string

---
+++ ProgrammingwithJavaScript/week2/string.js
@@ -0,0 +1,36 @@
+var letters = "abc";
+
+for(var i = 0; i<letters.length; i++){
+    console.log(letters[i]);
+}
+
+
+var veggies = ['onion', 'parsley', 'carrot'];
+console.log(veggies.length);
+
+console.log(veggies[0]);
+console.log(veggies[1]);
+
+
+for(var i = 0; i<veggies.length; i++){
+    console.log(veggies[i]);
+}
+
+
+var greeting = 'howdy';
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+
+for(var i = 0; i<greeting.length; i++){
+    console.log(greeting[i]);
+}
+
+var greet = 'Hello ';
+var user = 'Lisa'
+
+//console.log(greet.pop());
+console.log(greet + user);
+console.log(greet.concat(user));
