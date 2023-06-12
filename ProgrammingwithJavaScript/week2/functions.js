38eeb8e33907464b332a6ca1eed12ee0cf8cc367
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 13:00:48 +0200

    functions

---
+++ ProgrammingwithJavaScript/week2/functions.js
@@ -0,0 +1,22 @@
+function myFunc(){
+    console.log("Hello World!");
+}
+
+myFunc();
+
+
+//Add two numbers 
+function addTwoNums(){
+    var a = 10;
+var b = 20;
+var c = a + b;
+console.log(c);
+}
+addTwoNums();
+
+function addTwoNumbers(a, b){
+
+    var c = a + b;
+    console.log(c);
+}
+addTwoNumbers(5, 10);
