79f22baf008e3465eb0e5b790b51857201046ed9
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 13:53:39 +0200

    objects

---
+++ ProgrammingwithJavaScript/week2/object.js
@@ -0,0 +1,26 @@
+//storage manger
+var storMangerMovement = 4;
+var storMangerSocialSkills = 50;
+var storMangerStreetSmarts = 50;
+var storMangerHealth = 30;
+
+var storeManger= {};
+
+storeManger.rangeTitesPerTurn = 4;
+storeManger.socialSkills = 50;
+storeManger.streetSmarts = 50;
+storeManger.health = 30;
+storeManger.specialAbility = "finding business opportunities";
+storeManger.Healthgreeting = "Let's a make some money";
+storeManger.nextAchievement = "open a new store";
+
+var assistantManger = {
+    movement: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40
+}
+
+console.log(assistantManger);
+
+console.log(storeManger)
