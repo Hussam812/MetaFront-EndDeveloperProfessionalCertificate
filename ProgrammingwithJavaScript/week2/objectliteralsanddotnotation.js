19c84b46574a9a27286bf94da31fa2635f12d3ce
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 14:31:25 +0200

    object literals and the brackets notation

--- ProgrammingwithJavaScript/week2/objectliteralsanddotnotation.js
+++ ProgrammingwithJavaScript/week2/objectliteralsanddotnotation.js
@@ -14,9 +14,9 @@ var table = {
     priceUSD:100,
 }
 
-console.log(table);
+//console.log(table);
 
-console.log(table.color);
+//console.log(table.color);
 
 var house = {
     rooms: 3,
@@ -29,10 +29,40 @@ house2.rooms = 4;
 house2.color = "pink";
 house2.priceUSD = 12345;
 
-console.log(house);
+//console.log(house);
 house.windows = 10;
-console.log(house);
+//console.log(house);
 
 
 house.windows = 11;
-console.log(house)
+//console.log(house);
+
+//litterals and brackets Notation
+var house2 = {};
+house2["rooms"] = 4;
+house2['color'] = 'pink';
+house2['priceUSD'] = 123;
+console.log(house2);
+
+var car = {};
+car.color = "red";
+car['color'] = 'green';
+car['speed'] = 200;
+car.speed = 100;
+console.log(car);
+
+car['2022'] = 1901;
+console.log(car);
+
+
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed:100,
+    altitude: 200,
+    color: 'red'
+}
+
+
+for(var i = 0; i < arrOfKeys.length; i++){
+    console.log(drone[arrOfKeys[i]]);
+}
