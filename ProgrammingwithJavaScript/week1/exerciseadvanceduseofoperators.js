dba02eead6878636f6fac15bb58640175b733530
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 06:20:13 +0200

    exercise advanced use of operators

---
+++ ProgrammingwithJavaScript/week1/exerciseadvanceduseofoperators.js
@@ -0,0 +1,21 @@
+//Task 1
+var score = 8;
+console.log("Mid-level skills:", score > 0 && score < 10);
+//Task 2
+
+var timeRemaining = 0;
+var energy = 10;
+console.log("Game over: ", timeRemaining == 0 || energy == 0);
+
+//Task 3
+
+var num1 = 2; // 1.The frist varible, named num1, should be assigned a number value of 2.
+var num2 = 5; // 2.The second varible, named num2, should be assigned a number value of 5.
+var test1 = num1 % 2; // 3.The third varible, named test1, should be assigned the calculation of num1 % 2.Note:executing this cod4e will return a number.
+var test2 = num2 % 2; // 4.The fourth varible, named test2, should be assigned the calculation of num2 % 2.Note:executing this cod4e will return a number.
+
+var result1 = test1== 0; // 5.The fifth varinble, named result1, should be assigned the result of comparing if the number stored in the test1 varible is not equle to 0, in other words, test2 == 0.
+var result2 = test2 == 0;// 6.The sixth varinble, named result2, should be assigned the result of comparing if the number stored in the test2 varible is not equle to 0, in other words, test2 == 0.
+
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2);
