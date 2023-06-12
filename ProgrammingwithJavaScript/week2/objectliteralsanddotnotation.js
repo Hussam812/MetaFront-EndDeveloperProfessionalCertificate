a520d3941a83975b32234648ae2018f0256b22a8
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-12 14:17:41 +0200

    object literals and the dot notation

---
+++ ProgrammingwithJavaScript/week2/objectliteralsanddotnotation.js
@@ -0,0 +1,38 @@
+var user = {};
+var assistantManger ={
+    rangeTilesPerTurn: 3,
+    socialSkills:30,
+    streetSmarts:30,
+    health:40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make money"
+}
+
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD:100,
+}
+
+console.log(table);
+
+console.log(table.color);
+
+var house = {
+    rooms: 3,
+    color: "brown",
+    priceUSD: 10000,
+}
+
+var house2 = {};
+house2.rooms = 4;
+house2.color = "pink";
+house2.priceUSD = 12345;
+
+console.log(house);
+house.windows = 10;
+console.log(house);
+
+
+house.windows = 11;
+console.log(house)
