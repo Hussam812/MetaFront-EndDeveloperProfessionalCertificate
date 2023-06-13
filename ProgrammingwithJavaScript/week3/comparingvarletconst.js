9827eea1402eb588d93b02e4e4d7c3011978ddb1
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-13 14:40:10 +0200

    const, var & let compare

---
+++ ProgrammingwithJavaScript/week3/comparingvarletconst.js
@@ -0,0 +1,20 @@
+console.log(user);
+var user = 'Mary';
+var user = "Joanna";
+var user = "Mark";
+
+
+console.log(user);
+// var user;
+
+
+//console.log(usr);
+//let usr;
+
+let usr;
+console.log(usr);
+
+//let usr = "Anna";       //error
+
+usr = "Anna";
+console.log(usr);
