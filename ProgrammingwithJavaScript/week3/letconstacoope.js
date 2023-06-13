b4546357d33285a104492560f0550bb52c17a9da
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-13 14:29:28 +0200

    scope & let & const

---
+++ ProgrammingwithJavaScript/week3/letconstacoope.js
@@ -0,0 +1,19 @@
+var num1 = 10;
+function score(){
+    var num2 =20;
+    console.log(num2);
+}
+
+let color = "red";
+
+if ( color == "red"){
+    let color = "blue";
+    console.log(color);
+}
+
+console.log(color);
+
+
+var x = 1;
+var y = 2;
+var z = x + y;
